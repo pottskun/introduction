94e072b6c4a7ac5cc61bcb04e1f9590c9810f79f
Pottskun <wso.aphwx_vs.9@icloud.com>
2023-04-08 01:13:30 +0900

    カードを押したらカードが消えるカードが消えたら下からwindowが出てくるようにした

--- js/app.js
+++ js/app.js
@@ -3,29 +3,36 @@
 const topic = document.getElementsByClassName("topic")[0];
 topic.addEventListener("click", function () {
   topic.classList.add("msg-picup")
+
+
   // ここではアニメーションを取得してきてアニメーション終了後にイベントを実行している
   topic.addEventListener("animationend", () => {
-    // アニメーション終了後に実行する内容
-    let fragment = document.createDocumentFragment();
-    // ここではid="sec"というdivの中に新たに要素を作っている
-    const modal = document.getElementById("modal");
-    // ここにカードに出したいメッセージの配列を作る
-    let msgs = ["You", "are", "a", "genius"]
-    // fragmentのfor文を追加
-    for (let i = 0; i < msgs.length; i++) {
-      // 子要素を作成
-      const newMsg = document.createElement("p");
-      newMsg.classList.add("content")
-      newMsg.innerHTML = msgs[i];
-      // fragmentに追加
-      fragment.appendChild(newMsg);
-    }
-    // msg-sec.appendChild(fragment);
-    // ここがなぜmsg-secではダメなのかは後で理解を深める。
-    modal.appendChild(fragment);
-    // div.appendChild(fragment(msgs[i]));
+    // アニメーション終了後に実行する内容(下からwindowがでてくる)
+    // まずはmodalの子どもにdivをつけ、そのdivにクラス名slideUPをつけたい
+    const modal = document.createElement("div");
+    modal.className = "slideUP js-closeBtn"
+    document.getElementById("modal").appendChild(modal);
+
   })
 })
 
-// クラス名を変えて、配列を作ってそれを出せたらgithubにあげる。
-// 変更点はクラス名の変更とfragmentを使った文字の複製と配列を作り違う文字を出したこと。
+//     let fragment = document.createDocumentFragment();
+//     // ここではid="sec"というdivの中に新たに要素を作っている
+//     const modal = document.getElementById("modal");
+//     // ここにカードに出したいメッセージの配列を作る
+//     let msgs = ["You", "are", "a", "genius"]
+//     // fragmentのfor文を追加
+//     for (let i = 0; i < msgs.length; i++) {
+//       // 子要素を作成
+//       const newMsg = document.createElement("p");
+//       newMsg.classList.add("content")
+//       newMsg.innerHTML = msgs[i];
+//       // fragmentに追加
+//       fragment.appendChild(newMsg);
+//     }
+//     // msg-sec.appendChild(fragment);
+//     // ここがなぜmsg-secではダメなのかは後で理解を深める。
+//     modal.appendChild(fragment);
+//     // div.appendChild(fragment(msgs[i]));
+//   })
+// })
