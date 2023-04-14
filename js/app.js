623b46f7f5593e5fab0f17cb3049d6dd648e85e7
Pottskun <wso.aphwx_vs.9@icloud.com>
2023-04-15 00:21:27 +0900

    slideUpの中にメッセージを出してスクロールできるようにした

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 const topic = document.getElementsByClassName("topic")[0];
 topic.addEventListener("click", function () {
   topic.classList.add("msg-picup")
-  })
-
+})
 
 // ここではアニメーションを取得してきてアニメーション終了後にイベントを実行している
 topic.addEventListener("animationend", () => {
@@ -19,42 +18,40 @@ topic.addEventListener("animationend", () => {
     // closeBtnが押された時の処理を書くmodalにクラスを付与して、そのクラスが付いたらスライドダウンするようにする
     modal.classList.add("slideDown")
     // 9行目で行っているのと同じように、slideDownのアニメーションが終わったら、
-  modal.addEventListener("animationend", () => { 
-    // topicを再度出す(topicにクラスをつけてそのクラスに復活するアニメーションをつける)
-    // topic.classList.toggle("comeback");
-    topic.classList.toggle("msg-picup")
-    // slideUpを消す
-    modal.remove();
+    modal.addEventListener("animationend", () => {
+      topic.classList.toggle("msg-picup")
+      // slideUpを消している
+      modal.remove();
+    })
   })
-  //   addEventListener("click", function() {
-  //   ("modal").classList.toggle("comeback")
-  // })
+  // 一つのカードに関することに対して出てくるメッセージを配列で管理する
+  // hogeという変数の中にslideUp(クラス)をとってきている
+  const hoge = document.getElementsByClassName("slideUp")[0];
+  // ここにカードに出したいメッセージの配列を作る
+  let msgs = [
+    "ミルクチョコレート",
+    "納豆ご飯",
+    "海、波の音",
+    "洗い立ての布団",
+    "お土産",
+    "夏の匂い",
+    "朝方に聞こえる虫や鳥の鳴き声",
+    "長靴",
+    "いちご飴",
+    "ネイル"]
+  // fragmentのfor文を追加、msgsの数だけ繰り返す
+  for (let i = 0; i < msgs.length; i++) {
+    // newMsgを作り、中にp要素を作る
+    const newMsg = document.createElement("p");
+    // そのp要素をクラス名"content"にする
+    newMsg.classList.add("content")
+    // 1番から順に表示していく
+    newMsg.innerHTML = msgs[i];
+    modal.appendChild(newMsg);
+
+  }
 })
-})
 
-// 4/14
-
-// toggleでつけたクラス名で要素を「隠す」を実行。つけられていない状態でtopicをfadeoutさせる。
+// スクロールバー無くしたいかも
 
 
-
-//     let fragment = document.createDocumentFragment();
-//     // ここではid="sec"というdivの中に新たに要素を作っている
-//     const modal = document.getElementById("modal");
-//     // ここにカードに出したいメッセージの配列を作る
-//     let msgs = ["You", "are", "a", "genius"]
-//     // fragmentのfor文を追加
-//     for (let i = 0; i < msgs.length; i++) {
-//       // 子要素を作成
-//       const newMsg = document.createElement("p");
-//       newMsg.classList.add("content")
-//       newMsg.innerHTML = msgs[i];
-//       // fragmentに追加
-//       fragment.appendChild(newMsg);
-//     }
-//     // msg-sec.appendChild(fragment);
-//     // ここがなぜmsg-secではダメなのかは後で理解を深める。
-//     modal.appendChild(fragment);
-//     // div.appendChild(fragment(msgs[i]));
-//   })
-// })
