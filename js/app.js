e249ecb70aa09b014ad849ff26013dc585ac98b6
Pottskun <wso.aphwx_vs.9@icloud.com>
2023-04-22 23:53:20 +0900

    closeBtnだけスクロールせず固定配置した

--- js/app.js
+++ js/app.js
@@ -54,66 +54,78 @@ let want = [
   "キーボード",
   "トラックパッド"]
 
-  // ツイート方でいくか淡々方でいくか
-
 const topics = document.getElementsByClassName("topic");
 // for-of文を使ってtopicを取得した後の流れを書く
 for (let topic of topics) {
-  // console.log(topic);
   topic.addEventListener("click", function () {
-    topic.classList.add("msg-picup")
-  })
-  // }
-  console.log(topic);
-  // ここではアニメーションを取得してきてアニメーション終了後にイベントを実行している
-  topic.addEventListener("animationend", () => {
-    // アニメーション終了後に実行する内容(下からwindowがでてくる)
-    const modal = document.createElement("div");
-    modal.className = "slideUp"
-    document.getElementById("modal").appendChild(modal);
-    const closeBtn = document.createElement("button");
-    closeBtn.className = "js-closeBtn"
-    document.getElementsByClassName("slideUp")[0].appendChild(closeBtn);
-    closeBtn.innerHTML = ("close");
-    // closeボタンが押されたらモーダルがスライドダウンする
-    closeBtn.addEventListener("click", function () {
-      // closeBtnが押された時の処理を書くmodalにクラスを付与して、そのクラスが付いたらスライドダウンするようにする
-      modal.classList.add("slideDown")
-      // 9行目で行っているのと同じように、slideDownのアニメーションが終わったら、
-      modal.addEventListener("animationend", () => {
-        topic.classList.toggle("msg-picup")
-        // slideUpを消している
-        modal.remove();
-      })
-    })
+    for (let otherTopic of topics) {
+      // otherTopicという変数を作ってその変数の中にtopicsの配列を入れる
+      if (otherTopic !== topic) {
+        // otherTopic と topic の値が異なる場合に true を返します。???
+        otherTopic.classList.add("msg-picup");
+      }
+    }
+    topic.classList.add("msg-picup");
+    // });
+    // }
 
-    // 一つのカードに関することに対して出てくるメッセージを配列で管理する
-    // hogeという変数の中にslideUp(クラス)をとってきている
-    const hoge = document.getElementsByClassName("slideUp")[0];
-    let content = [];
-    if (topic === topics[0]) {
-      content = like
-    } else if (topic === topics[1]) {
-      content = dislike;
-    } else if (topic === topics[2]) {
-      content = hobby;
-    } else if (topic === topics[3]) {
-      content = hello;
-    } else if (topic === topics[4]) {
-      content = study;
-    } else if (topic === topics[5]) {
-      content = want;
-    }
-    for (let i = 0; i < content.length; i++) {
-      const newMsg = document.createElement("p");
-      newMsg.classList.add("content")
-      newMsg.innerHTML = content[i];
-      modal.appendChild(newMsg);
-    }
-  })
+
+    // ここではアニメーションを取得してきてアニメーション終了後にイベントを実行している
+    topic.addEventListener("animationend", () => {
+      // アニメーション終了後に実行する内容(下からwindowがでてくる)
+      const modal = document.createElement("div");
+      modal.className = "slideUp"
+      document.getElementById("modal").appendChild(modal);
+      const closeBtn = document.createElement("button");
+      closeBtn.className = "js-closeBtn"
+      document.getElementsByClassName("slideUp")[0].appendChild(closeBtn);
+      closeBtn.innerHTML = ("close");
+      // closeボタンが押されたらモーダルがスライドダウンする
+      closeBtn.addEventListener("click", function () {
+        // closeBtnが押された時の処理を書くmodalにクラスを付与して、そのクラスが付いたらスライドダウンするようにする
+        modal.classList.add("slideDown")
+        // 9行目で行っているのと同じように、slideDownのアニメーションが終わったら、
+        modal.addEventListener("animationend", () => {
+          for (let removeTopic of topics) {
+            removeTopic.classList.remove("msg-picup")
+          }
+          // slideUpを消している
+          modal.remove();
+        })
+      })
+
+      // 一つのカードに関することに対して出てくるメッセージを配列で管理する
+      // hogeという変数の中にslideUp(クラス)をとってきている
+      const hoge = document.getElementsByClassName("slideUp")[0];
+      let content = [];
+      if (topic === topics[0]) {
+        content = like
+      } else if (topic === topics[1]) {
+        content = dislike;
+      } else if (topic === topics[2]) {
+        content = hobby;
+      } else if (topic === topics[3]) {
+        content = hello;
+      } else if (topic === topics[4]) {
+        content = study;
+      } else if (topic === topics[5]) {
+        content = want;
+      }
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("wrapper")
+      modal.appendChild(wrapper);
+      for (let i = 0; i < content.length; i++) {
+        const newMsg = document.createElement("p");
+        newMsg.classList.add("content")
+        newMsg.innerHTML = content[i];
+        wrapper.appendChild(newMsg);
+        // モーダルの子要素として追加
+      }
+    },{once: true})
+  });
 }
 
-
-
-
-
+// クラス名の変更とかその他やらないといけないことの整理23日
+// closeBtnが固定されない件について解決
+// 時間によって背景画像を変える22日
+// close押さないと消えないのうざいな23,24,25日
